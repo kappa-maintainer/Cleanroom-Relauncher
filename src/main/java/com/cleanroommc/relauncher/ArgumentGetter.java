94ccc839df31461422a02b36783a2c91b4b0073b
arcade_kappa <gkappa@outlands.top>
2025-04-24 18:27:42 +0800

    Fix URL space problem

--- src/main/java/com/cleanroommc/relauncher/ArgumentGetter.java
+++ src/main/java/com/cleanroommc/relauncher/ArgumentGetter.java
@@ -4,28 +4,29 @@ import net.minecraft.launchwrapper.LaunchClassLoader;
 import net.minecraftforge.fml.ExitWrapper;
 
 import java.io.File;
-import java.net.URL;
+import java.net.URI;
+import java.net.URISyntaxException;
 import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
 
 public class ArgumentGetter {
     public static List<String> getLaunchArgs() {
-        URL vanillaJar = null;
+        URI vanillaJar = null;
         try {
-            vanillaJar = Class.forName("net.minecraft.client.ClientBrandRetriever", false, LaunchClassLoader.class.getClassLoader()).getProtectionDomain().getCodeSource().getLocation();
-        } catch (ClassNotFoundException e) {
+            vanillaJar = new URI(Class.forName("net.minecraft.client.ClientBrandRetriever", false, LaunchClassLoader.class.getClassLoader()).getProtectionDomain().getCodeSource().getLocation().toString());
+        } catch (ClassNotFoundException | URISyntaxException e) {
             Relauncher.LOGGER.error("Can't get vanilla jar, impossible to relaunch");
             ExitWrapper.exit(1);
         }
-        Relauncher.LOGGER.info("Vanilla jar detected: {}", vanillaJar.getFile());
+        Relauncher.LOGGER.info("Vanilla jar detected: {}", vanillaJar.getPath());
         List<String> result = new ArrayList<>();
         result.add(Config.javaPath);
         if (!Config.jvmArgs.isEmpty()) {
             result.addAll(Arrays.asList(Config.jvmArgs.split(" ")));
         }
         result.add("-cp");
-        result.add(MMCPackParser.getClassPath() + new File(vanillaJar.getFile()));
+        result.add(MMCPackParser.getClassPath() + new File(vanillaJar.getPath()));
         List<String> origin = new ArrayList<>(Arrays.asList(System.getProperty("sun.java.command").split(" ")));
         origin.remove(0);
         origin.add(0, "top.outlands.foundation.boot.Foundation");
